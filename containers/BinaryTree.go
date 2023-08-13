f0397e848e52c723ea32d6a864d2444850d6bd8e
Al Degenaar <adegenaar@degenaar.org>
2023-08-12 21:11:19 -0400

    Deletion working

--- containers/BinaryTree.go
+++ containers/BinaryTree.go
@@ -1,6 +1,7 @@
 package containers
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -37,6 +38,28 @@ func (n *binaryNode[T]) addNode(cur *binaryNode[T]) {
 	}
 }
 
+func (n *binaryNode[T]) searchNode(val T) *binaryNode[T] {
+	if n == nil {
+		return nil
+	}
+
+	if n.data < val {
+		if n.right == nil {
+			return nil
+		}
+		return n.right.searchNode(val)
+	}
+	if n.data > val {
+		if n.left == nil {
+			return nil
+		}
+		return n.left.searchNode(val)
+	}
+
+	// found it
+	return n
+}
+
 func (t *binaryNode[T]) walk(n *binaryNode[T], f func(T)) {
 	if n == nil {
 		return
@@ -44,6 +67,66 @@ func (t *binaryNode[T]) walk(n *binaryNode[T], f func(T)) {
 	t.walk(n.left, f)
 	f(n.data)
 	t.walk(n.right, f)
+}
+
+func (n *binaryNode[T]) removeNode(value T, parent *binaryNode[T]) error {
+	//Delete node from binary search tree
+	if n == nil {
+		return errors.New("value does not exist")
+	}
+	if value < n.data {
+		return n.left.removeNode(value, n)
+	}
+	if value > n.data {
+		return n.right.removeNode(value, n)
+	}
+	// must be equal...
+
+	// check for the simple case where there are no children
+	if n.left == nil && n.right == nil {
+		n.replaceNode(parent, nil)
+		return nil
+	}
+
+	// check for the case where there is only one child
+	if n.left == nil {
+		n.replaceNode(parent, n.right)
+		return nil
+	}
+	if n.right == nil {
+		n.replaceNode(parent, n.left)
+		return nil
+	}
+
+	// there are two children, so replace by the maximum valued child
+	replacement, replParent := n.left.findMax(n)
+	n.data = replacement.data
+
+	return replacement.removeNode(replacement.data, replParent)
+}
+
+// find the child node with the maximum value
+func (n *binaryNode[T]) findMax(parent *binaryNode[T]) (*binaryNode[T], *binaryNode[T]) {
+	if n == nil {
+		return nil, parent
+	}
+	if n.right == nil {
+		return n, parent
+	}
+	return n.right.findMax(n)
+}
+
+func (n *binaryNode[T]) replaceNode(parent, replacement *binaryNode[T]) error {
+	if n == nil {
+		return errors.New("replaceNode() not allowed on a nil node")
+	}
+
+	if n == parent.left {
+		parent.left = replacement
+		return nil
+	}
+	parent.right = replacement
+	return nil
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -54,6 +137,9 @@ type Tree[T ~int | ~float64 | ~string] struct {
 }
 
 func (t *Tree[T]) Insert(value T) {
+	if t == nil {
+		return
+	}
 	n := newNode(value)
 	if t.root == nil {
 		t.root = n
@@ -63,11 +149,47 @@ func (t *Tree[T]) Insert(value T) {
 	t.length++
 }
 
+func (t *Tree[T]) Delete(value T) error {
+	if t == nil {
+		return errors.New("delete not allowed on an uninitialized tree")
+	}
+	if t.root == nil {
+		return errors.New("delete not allowed on an empty tree")
+	}
+
+	fakeParent := &binaryNode[T]{right: t.root}
+	err := t.root.removeNode(value, fakeParent)
+	if err != nil {
+		return err
+	}
+
+	if fakeParent.right == nil {
+		t.root = nil
+	}
+	t.length--
+	return nil
+}
+
+func (t *Tree[T]) Search(value T) *T {
+	if t == nil || t.root == nil {
+		return nil
+	}
+
+	n := t.root.searchNode(value)
+	return &n.data
+}
+
 func (t *Tree[T]) Walk(f func(T)) {
+	if t == nil || t.root == nil {
+		return
+	}
 	t.root.walk(t.root, f)
 }
 
 func (t *Tree[T]) PrettyPrint() {
+	if t == nil || t.root == nil {
+		return
+	}
 	printNode := func(n *binaryNode[T], depth int) {
 		fmt.Printf("%s%v\n", strings.Repeat("  ", depth), n.data)
 	}
--- containers/BinaryTree_test.go
+++ containers/BinaryTree_test.go
@@ -10,6 +10,7 @@ func TestBinaryTree_InsertAndTraverse(t *testing.T) {
 	expected := []string{"d", "b", "g", "g", "c", "e", "a", "h", "f", "i", "j", "l", "k"}
 
 	tree := &Tree[string]{}
+	tree.PrettyPrint()
 	for i := 0; i < len(expected); i++ {
 		tree.Insert(expected[i])
 	}
@@ -21,4 +22,146 @@ func TestBinaryTree_InsertAndTraverse(t *testing.T) {
 	if !reflect.DeepEqual(values, expected) {
 		t.Errorf("Expected %v, got %v", expected, values)
 	}
+}
+
+func TestBinaryTree_Walk(t *testing.T) {
+	expected := []string{"d", "b", "g", "g", "c", "e", "a", "h", "f", "i", "j", "l", "k"}
+
+	tree := &Tree[string]{}
+
+	// check for walking the empty tree
+	values := []string{}
+	tree.Walk(func(n string) { values = append(values, n) })
+	if len(values) != 0 {
+		t.Errorf("Expected %v, got %v", 0, len(values))
+	}
+
+	// insert the expected values
+	for i := 0; i < len(expected); i++ {
+		tree.Insert(expected[i])
+	}
+
+	// walk the tree and validate the results
+	values = []string{}
+	tree.Walk(func(n string) { values = append(values, n) })
+	sort.Strings(expected)
+	if !reflect.DeepEqual(values, expected) {
+		t.Errorf("Expected %v, got %v", expected, values)
+	}
+}
+func TestBinaryTree_Search(t *testing.T) {
+	expected := []string{"d", "b", "g", "g", "c", "e", "a", "h", "f", "i", "j", "l", "k"}
+
+	tree := &Tree[string]{}
+	v := tree.Search("x")
+	if v != nil {
+		t.Errorf("Expected %v, got %v", nil, v)
+	}
+	for i := 0; i < len(expected); i++ {
+		tree.Insert(expected[i])
+	}
+	values := []string{}
+	for i := 0; i < len(expected); i++ {
+		values = append(values, *tree.Search(expected[i]))
+	}
+	sort.Strings(values)
+	sorted := make([]string, len(expected))
+	copy(sorted, expected)
+
+	sort.Strings(sorted)
+	if !reflect.DeepEqual(values, sorted) {
+		t.Errorf("Expected %v, got %v", sorted, values)
+	}
+
+	values = []string{}
+	for i := len(expected) - 1; i >= 0; i-- {
+		values = append(values, *tree.Search(expected[i]))
+	}
+	sort.Strings(values)
+	sort.Strings(expected)
+	if !reflect.DeepEqual(values, expected) {
+		t.Errorf("Expected %v, got %v", expected, values)
+	}
+}
+
+func TestBinaryTree_Delete(t *testing.T) {
+	expected := []string{"d", "b", "g", "g", "c", "e", "a", "h", "f", "i", "j", "l", "k"}
+
+	tree := &Tree[string]{}
+	ret := tree.Delete("x")
+	if ret == nil {
+		t.Errorf("Expected %v, got %v", "delete not allowed on an empty tree", ret)
+	}
+	tree.Insert(expected[0])
+	ret = tree.Delete("x")
+	if ret == nil {
+		t.Errorf("Expected %v, got %v", "value does not exist", ret)
+	}
+	ret = tree.Delete(expected[0])
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+
+	tree.Insert(expected[0])
+	tree.Insert(expected[1])
+	ret = tree.Delete(expected[0])
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+	ret = tree.Delete(expected[1])
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+	tree.Insert(expected[0])
+	tree.Insert(expected[1])
+	ret = tree.Delete(expected[1])
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+	ret = tree.Delete(expected[0])
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+	for i := 0; i < len(expected); i++ {
+		tree.Insert(expected[i])
+	}
+	for i := 0; i < len(expected); i++ {
+		if err := tree.Delete(expected[i]); err != nil {
+			t.Errorf("Expected %v, got %v", nil, err)
+		}
+	}
+	if tree.length != 0 {
+		t.Errorf("Expected %v, got %v", 0, tree.length)
+	}
+}
+
+func TestBinaryTree_NilChecks(t *testing.T) {
+	var tree *Tree[string] = nil
+
+	tree.Insert("x")
+	err := tree.Delete("x")
+	if err == nil {
+		t.Errorf("Expected %v, got %v", "delete not allowed on an empty tree", err)
+	}
+	tree.Search("x")
+	tree.Walk(func(n string) {})
+	tree.PrettyPrint()
+
+	var node *binaryNode[string] = nil
+
+	ret, _ := node.findMax(node)
+	if ret != nil {
+		t.Errorf("Expected %v, got %v", nil, ret)
+	}
+	err = node.replaceNode(node, nil)
+	if err == nil {
+		t.Errorf("Expected %v, got %v", "replaceNode() not allowed on a nil node", err)
+	}
+
+	node = node.searchNode("x")
+	if node != nil {
+		t.Errorf("Expected %v, got %v", nil, node)
+	}
+
+	node.addNode(node)
 }
